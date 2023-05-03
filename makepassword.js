6a89612e6be45e6d5a970e335559333c62dd4767
ScramblesDev <beesley02@gmail.com>
2023-05-03 17:09:10 -0400

    Update makepassword.js

--- makepassword.js
+++ makepassword.js
@@ -7,5 +7,21 @@ const hash_algo = 'sha256';
 
 //generating our password
 function generatePassword(password_file, encrypted_file) {
-    
+
+    //defining the file
+    const data = fs.readFileSync(password_file, 'utf8');
+    const lines = data.trim().split('\n');
+    const encrypted_stream = fs.createWriteStream(encrypted_file);
+
+    //using the examples, splitting it together
+    for (let line of lines) {
+        const [email, password] = line.split(':');
+        const hash = crypto.createHash(hash_algo).update(password).digest('hex');
+        encrypted_stream.write(`${email}:${hash}\n`);
+    }
+    encrypted_stream.end();
 }
+
+//basically puts the generation into effect
+if (require.main === module) { generatePassword('./password.txt', './password.enc.txt'); }
+module.exports = { generatePassword };
